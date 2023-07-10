a9fb3baf8dc70aabb0b387d851945e9d3d62bc5c
Ukaypromise <promiseukaimo@gmail.com>
2023-07-10 20:40:38 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="What's your goal..." style={styles.textInput} />
+        <Button title="Add Goal" color="#841584" />
+      </View>
+      <View style={styles.goalContainer}>
+        <Text style={styles.goalText}>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5FCFF",
+    padding: 50,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#841584",
+    borderRadius: 5,
+    width: "80%",
+    marginRight: 10,
+    padding: 8,
+    fontSize: 16,
+  },
+  goalContainer: {
+    flex:3,
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#841584",
+    borderRadius: 5,
+  },
+  goalText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
